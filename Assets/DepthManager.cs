63ef224c4de8399ec6667d359bfdeeb17bdcb445
Jiajian Zhang <handsomeJian@live.com>
2024-02-05 00:34:35 -0500

    Hand Mask

---
+++ Assets/DepthManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Meta.XR.Depth;
+
+public class DepthManager : MonoBehaviour
+{
+    private EnvironmentDepthTextureProvider _depthTextureProvider;
+
+    private void Awake()
+    {
+        _depthTextureProvider = GetComponent<EnvironmentDepthTextureProvider>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _depthTextureProvider.RemoveHands(true);
+    }
+}
