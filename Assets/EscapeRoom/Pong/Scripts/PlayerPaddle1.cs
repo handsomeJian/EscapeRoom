68f64903c74a64ec59ab9249429d4e2354b088a7
TingruLian <tinalian@bu.edu>
2024-02-05 20:18:05 -0500

    pong

---
+++ Assets/EscapeRoom/Pong/Scripts/PlayerPaddle1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerPaddle1 : Paddle
+{
+    private Vector2 direction;
+
+    private void Update()
+    {
+        if ( Input.GetKey(KeyCode.UpArrow))//Input.GetKey(KeyCode.W) ||
+        {
+            direction = Vector2.up;
+        }
+        else if ( Input.GetKey(KeyCode.DownArrow))//Input.GetKey(KeyCode.S) ||
+        {
+            direction = Vector2.down;
+        }
+        else
+        {
+            direction = Vector2.zero;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (direction.sqrMagnitude != 0)
+        {
+            rb.AddForce(direction * speed);
+        }
+    }
+
+}
