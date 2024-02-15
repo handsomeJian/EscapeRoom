9b82bd0008c8ef545ce8b034acff3eeab70ebbc3
TingruLian <tinalian@bu.edu>
2024-02-15 17:40:24 -0500

    distance

--- Assets/EscapeRoom/Pong/Scripts/PlayerPaddle.cs
+++ Assets/EscapeRoom/Pong/Scripts/PlayerPaddle.cs
@@ -1,12 +1,31 @@
 ﻿using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerPaddle : Paddle
 {
     private Vector3 direction;
     public float fixedX = 0f;
+    [SerializeField] private Transform MaxPos, MinPos;
+    public int dist = 0;
+    public int minDis = 5, maxDis = 60;
+
+    void Start()
+    {
+        
+    }
     private void Update()
     {
-        Vector3 currentPosition = transform.localPosition;
+
+        //Vector3 currentPosition = transform.localPosition;
+
+
+        var moveVec = MaxPos.position - MinPos.position;
+        float w = (dist - minDis) * 1.0f / (maxDis - minDis);
+        var newPos = MinPos.position + moveVec * w;
+        transform.position = newPos;
+
+
+        /*
         transform.localPosition = new Vector3(fixedX, currentPosition.y, currentPosition.z);
         if (Input.GetKey(KeyCode.W) ) {//|| Input.GetKey(KeyCode.UpArrow)
             direction = transform.parent.TransformDirection(Vector3.up);
@@ -15,13 +34,16 @@ public class PlayerPaddle : Paddle
         } else {
             direction = Vector3.zero;
         }
+        */
     }
 
     private void FixedUpdate()
     {
+        /*
         if (direction.sqrMagnitude != 0) {
             rb.AddForce(direction * speed);
         }
+        */
     }
 
 }
