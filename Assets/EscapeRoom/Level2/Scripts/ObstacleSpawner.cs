fdbdf0484e6250a7a92f4f4092de70c82e0f996a
TingruLian <tinalian@bu.edu>
2024-03-21 16:16:38 -0400

    animation

--- Assets/EscapeRoom/Level2/Scripts/ObstacleSpawner.cs
+++ Assets/EscapeRoom/Level2/Scripts/ObstacleSpawner.cs
@@ -3,25 +3,26 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject obstaclePrefab; // Assign in inspector
-    [SerializeField] private float spawnRate = 3.0f; // Time between spawns
+    [SerializeField] private GameObject obstaclePrefab, computerfile; // Assign in inspector
+    [SerializeField] private float spawnRate = 3.0f, animationTime = 0.08f; // Time between spawns
     [SerializeField] private Transform[] spawnPoints; // Assign spawn points in inspector
     [SerializeField] private Animator JJlevel2Anim;
 
     private float nextSpawnTime;
+   // private Animation anim;
 
     [SerializeField] private Sprite[] sprites;
     void Update()
     {
         if (Time.time >= nextSpawnTime)
         {
-            SpawnObstacle();
+            
             PlayAttackAnim();
             nextSpawnTime = Time.time + 1f / spawnRate;
         }
     }
 
-    void SpawnObstacle()
+    void SpawnObstacle( int randomIndex)
     {
         int spawnIndex = Random.Range(0, spawnPoints.Length);
         var obstacle = Instantiate(obstaclePrefab, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation);
@@ -32,20 +33,35 @@ public class ObstacleSpawner : MonoBehaviour
             var spriteRenderer = child.GetComponent<SpriteRenderer>();
             if (spriteRenderer != null)
             {
-                spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+                spriteRenderer.sprite = sprites[randomIndex];
+
             }
         }
 
         obstacle.SetActive(true);
+
     }
     void PlayAttackAnim()
     {
+        //change animation sprite
+        int randomIndex = Random.Range(0, sprites.Length);
+        computerfile.GetComponent<SpriteRenderer>().sprite = sprites[randomIndex];
+        if (JJlevel2Anim.gameObject.activeSelf == false)
+        {
+            JJlevel2Anim.gameObject.SetActive(true);
+        }
+
+        ;
         JJlevel2Anim.SetBool("isAttack", true);
-        StartCoroutine(PlayIdle(1f));
+        //JJlevel2Anim.SetTrigger("Attack");
+        StartCoroutine(PlayIdle(animationTime, randomIndex));
     }
-    IEnumerator PlayIdle(float delay)
+    IEnumerator PlayIdle(float delay, int randomIndex)
     {
         yield return new WaitForSeconds(delay);
         JJlevel2Anim.SetBool("isAttack", false);
+        //JJlevel2Anim.SetTrigger("Idle");
+        yield return new WaitForSeconds(1f);
+        SpawnObstacle(randomIndex);
     }
 }
