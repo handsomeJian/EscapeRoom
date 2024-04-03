7bcbb100676c79ce4b00a3bffe08a87d2d5726db
TingruLian <tinalian@bu.edu>
2024-04-03 14:30:47 -0400

    update level 3

--- Assets/L3GameManager.cs
+++ Assets/L3GameManager.cs
@@ -31,26 +31,34 @@ public class L3GameManager : MonoBehaviour
 
         // Move to the next position in the array, looping back to the start if necessary
         currentPositionIndex = (currentPositionIndex + 1) % positions.Length;
-        L3JJ.transform.position = positions[currentPositionIndex].position + new Vector3(0,0.6f,0);
+        L3JJ.transform.position = positions[currentPositionIndex].position;// + new Vector3(0,0.2f,0);
     }
 
     IEnumerator TeleportAfterVFX()
     {
         // Exit if no positions are set
         if (positions.Length == 0) yield break;
-
+        
         // Play the teleport VFX
         teleportVFX.SetActive(true);
         //teleportVFX.GetComponent<VisualEffect>().Play();
 
 
         // Wait for the VFX to finish
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(.8f);
+        if (currentPositionIndex == 2)
+        {
+            L3JJ.SetActive(false);
+        }
+        else
+        { // Move to the next position in the array, looping back to the start if necessary
+            currentPositionIndex = (currentPositionIndex + 1) % positions.Length;
+            L3JJ.transform.position = positions[currentPositionIndex].position + new Vector3(0, 0.6f, 0);
+           //yield return new WaitForSeconds(.2f);
+            teleportVFX.SetActive(false);
 
-        // Move to the next position in the array, looping back to the start if necessary
-        currentPositionIndex = (currentPositionIndex + 1) % positions.Length;
-        L3JJ.transform.position = positions[currentPositionIndex].position + new Vector3(0, 0.6f, 0);
-        teleportVFX.SetActive(false);
+        }
+       
     }
 
 }
