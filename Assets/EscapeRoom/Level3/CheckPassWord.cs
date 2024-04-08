6f04028c639895e3cf4adf0b7956ecd174727a6e
TingruLian <tinalian@bu.edu>
2024-04-08 14:40:42 -0400

    update

--- Assets/EscapeRoom/Level3/CheckPassWord.cs
+++ Assets/EscapeRoom/Level3/CheckPassWord.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Runtime.InteropServices.ComTypes;
 
 public class CheckPassWord : MonoBehaviour
 {
@@ -12,15 +13,37 @@ public class CheckPassWord : MonoBehaviour
     [SerializeField] private string correctString = "handsomeJJ";
     [SerializeField] private GameObject warning;
     [SerializeField] private AudioSource wrongBuzz;
-
+    [SerializeField] private RectTransform targetArea1, targetArea2; // The area where the click should not trigger the audio
     public UdpSocketSender sender;
+    private bool isArea1;
     void Start()
     {
+        isArea1 = true;
         escapeButton.onClick.AddListener(CheckInput);
         closeBotton.onClick.AddListener(CloseWarning);
     }
 
-
+    private void Update()
+    {
+        // Check for mouse click
+        if (Input.GetMouseButtonDown(0)&& isArea1)//&& !IsPointerOverUIObject()
+        {
+            // Check if the click is outside the target area
+            if (!RectTransformUtility.RectangleContainsScreenPoint(targetArea1, Input.mousePosition))
+            {
+                // Play the audio
+                wrongBuzz.Play();
+            }
+        }
+        else if (Input.GetMouseButtonDown(0) && !isArea1)
+        {
+            if (!RectTransformUtility.RectangleContainsScreenPoint(targetArea2, Input.mousePosition))
+            {
+                // Play the audio
+                wrongBuzz.Play();
+            }
+        }
+    }
     void CheckInput()
     {
 
@@ -29,12 +52,14 @@ public class CheckPassWord : MonoBehaviour
 
         if (userInput.Equals(correctString))
         {
+            isArea1 = true;
             Debug.Log("Input matches the specific string!");
             sender.SendMessage("Pass");
             
         }
         else
         {
+            isArea1 = false;
             Debug.Log("Input does not match.");
             userInputField.text = "";
             warning.SetActive(true);
@@ -45,5 +70,6 @@ public class CheckPassWord : MonoBehaviour
     void CloseWarning()
     {
         warning.SetActive(false);
+        isArea1 = true;
     }
 }
